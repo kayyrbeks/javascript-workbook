206937d8e5ae67945bbab86c78a7cb61c665ab46
Kayyrbek Perdebekov <kperdebekoff@gmail.com>
2023-06-04 23:15:41 +0500

    feat: add task solution

---
+++ 01-intro-exercises/003-room-area/src/index.js
@@ -0,0 +1,6 @@
+import computeRoomArea from "./computeRoomArea.js";
+
+const roomWidth = prompt("Enter the width of a room:");
+const roomLength = prompt("Enter the length of a romm:");
+
+console.log(computeRoomArea(roomWidth, roomLength));
