222c9b36b24ceac8e7c403e64def2ccc5864cec3
Kayyrbek Perdebekov <kperdebekoff@gmail.com>
2023-06-05 18:04:40 +0500

    refactor: add function to convert input value to number

--- 01-intro-exercises/003-room-area/src/index.js
+++ 01-intro-exercises/003-room-area/src/index.js
@@ -1,6 +1,6 @@
 import computeRoomArea from "./computeRoomArea.js";
 
-const roomWidth = prompt("Enter the width of a room:");
-const roomLength = prompt("Enter the length of a romm:");
+const roomWidth = Number(prompt("Enter the width of a room:"));
+const roomLength = Number(prompt("Enter the length of a romm:"));
 
 console.log(computeRoomArea(roomWidth, roomLength));
